fbf8056086d5c9317dda34ee03ef0be81069a95a
NRNagaphanideep <phanideep14@gmail.com>
2023-02-23 13:17:35 +0000

    all test cases passed

---
+++ src/components/AppItem/index.js
@@ -0,0 +1,51 @@
+import {Component} from 'react'
+import './index.css'
+
+class AppItem extends Component {
+  state = {isClicked: false, enter: ''}
+
+  one = event => {
+    this.setState({enter: event.target.value})
+  }
+
+  start = () => {
+    this.setState(prevState => ({
+      isClicked: !prevState.isClicked,
+    }))
+  }
+
+  render() {
+    const {isClicked, enter} = this.state
+    return (
+      <div className="main-container">
+        <div className="content-container">
+          <h1 className="header">Editable Text Input</h1>
+          <div className="input-container">
+            {isClicked === true ? (
+              <div className="paragraph-container">
+                <p className="paragraph">{enter}</p>
+                <button type="button" className="button" onClick={this.start}>
+                  Edit
+                </button>
+              </div>
+            ) : (
+              <>
+                <input
+                  type="text"
+                  className="input"
+                  value={enter}
+                  onChange={this.one}
+                />
+                <button type="button" className="button" onClick={this.start}>
+                  Save
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AppItem
